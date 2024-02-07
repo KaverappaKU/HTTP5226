2593c1f09460da46771371e7762fc80c2ce4cca8
KaverappaKU <caakash357@gmail.com>
2024-02-07 14:01:55 -0500

    added Game CRUD operations

--- Games_Catalog_N01589651/Games_Catalog_N01589651/Controllers/GameController.cs
+++ Games_Catalog_N01589651/Games_Catalog_N01589651/Controllers/GameController.cs
@@ -45,20 +45,200 @@ namespace Games_Catalog_N01589651.Controllers
         // GET: Game/Details/2
         public ActionResult Details(int id)
         {
-            //objective: communicate with our animal data api to retrieve one animal
-            //curl https://localhost:44370/api/gamedata/listgames/{id}
+            //DetailsGames ViewModel = new DetailsGames();
 
-            string url = "findgame/" + id;
+            //objective: communicate with our animal data api to retrieve one game
+            //curl https://localhost:44324/api/animaldata/findanimal/{id}
+
+            string url = "gamedata/findgame/" + id;
             HttpResponseMessage response = client.GetAsync(url).Result;
 
             Debug.WriteLine("The response code is ");
             Debug.WriteLine(response.StatusCode);
 
-            GameDto selectedGame = response.Content.ReadAsAsync<GameDto>().Result;
-            Debug.WriteLine(selectedGame.GameName);
+            GameDto SelectedGame = response.Content.ReadAsAsync<GameDto>().Result;
+
+            //ViewModel.SelectedGame = SelectedGame;
+
+            //url = "developerdata/listdevelopersforgame/" + id;
+            //response = client.GetAsync(url).Result;
+            //IEnumerable<DeveloperDto> ResponsibleDevelopers = response.Content.ReadAsAsync<IEnumerable<DeveloperDto>>().Result;
+
+            //ViewModel.ResponsibleDevelopers = ResponsibleDevelopers;
+
+            //url = "developerdata/listdevelopersnotforgame/" + id;
+            //response = client.GetAsync(url).Result;
+            //IEnumerable<DeveloperDto> AvailableDevelopers = response.Content.ReadAsAsync<IEnumerable<DeveloperDto>>().Result;
+
+            //ViewModel.AvailableDevelopers = AvailableDevelopers;
 
 
-            return View(selectedGame);
+            return View(SelectedGame);
+        }
+
+
+        //POST: Game/Associate/{gameid}
+        [HttpPost]
+        public ActionResult Associate(int id, int DeveloperID)
+        {
+            Debug.WriteLine("Attempting to associate game :" + id + " with developer " + DeveloperID);
+
+            //call our api to associate game with developer
+            string url = "gamedata/associategamewithdeveloper/" + id + "/" + DeveloperID;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            return RedirectToAction("Details/" + id);
+        }
+
+
+        //Get: Game/UnAssociate/{id}?DeveloperID={developerID}
+        [HttpGet]
+        public ActionResult UnAssociate(int id, int DeveloperID)
+        {
+            Debug.WriteLine("Attempting to unassociate game :" + id + " with developer: " + DeveloperID);
+
+            //call our api to associate game with developer
+            string url = "gamedata/unassociategamewithdeveloper/" + id + "/" + DeveloperID;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            return RedirectToAction("Details/" + id);
+        }
+
+
+        public ActionResult Error()
+        {
+
+            return View();
+        }
+
+        // GET: Animal/New
+        public ActionResult New()
+        {
+            //information about all genre in the system.
+            //GET api/genredata/listgenre
+
+            //string url = "genredata/listgenre";
+            //HttpResponseMessage response = client.GetAsync(url).Result;
+            //IEnumerable<GenreDto> SpeciesOptions = response.Content.ReadAsAsync<IEnumerable<GenreDto>>().Result;
+
+            //return View(GenreOptions);
+            return View();
+        }
+
+        // POST: Game/Create
+        [HttpPost]
+        public ActionResult Create(Game game)
+        {
+            Debug.WriteLine("the json payload is :");
+            //Debug.WriteLine(animal.AnimalName);
+            //objective: add a new animal into our system using the API
+            //curl -H "Content-Type:application/json" -d @animal.json https://localhost:44324/api/animaldata/addanimal 
+            string url = "gamedata/addgame";
+
+
+            string jsonpayload = jss.Serialize(game);
+            Debug.WriteLine(jsonpayload);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+
+        }
+
+        // GET: Animal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            //UpdateGame ViewModel = new UpdateGame();
+
+            ////the existing game information
+            //string url = "gamedata/findgame/" + id;
+            //HttpResponseMessage response = client.GetAsync(url).Result;
+            //GameDto SelectedGame = response.Content.ReadAsAsync<GameDto>().Result;
+            //ViewModel.SelectedGame = SelectedGame;
+
+            //// all genre to choose from when updating this game
+            ////the existing game information
+            //url = "genredata/listgenre/";
+            //response = client.GetAsync(url).Result;
+            //IEnumerable<GenreDto> GenreOptions = response.Content.ReadAsAsync<IEnumerable<GenreDto>>().Result;
+
+            //ViewModel.GenreOptions = GenreOptions;
+
+            //return View(ViewModel);
+            string url = "findgame/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            //Debug.WriteLine("The response code is ");
+            //Debug.WriteLine(response.StatusCode);
+
+            GameDto selectedgame = response.Content.ReadAsAsync<GameDto>().Result;
+            //Debug.WriteLine("animal received : ");
+            //Debug.WriteLine(selectedanimal.AnimalName);
+
+            return View(selectedgame);
+        }
+
+        // POST: Game/Update/5
+        [HttpPost]
+        public ActionResult Update(int id, Game animal)
+        {
+
+            string url = "Gamedata/updategame/" + id;
+            string jsonpayload = jss.Serialize(animal);
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            Debug.WriteLine(content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        // GET: Game/Delete/3
+        public ActionResult DeleteConfirm(int id)
+        {
+            string url = "gamedata/findgame/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            GameDto selectedgame = response.Content.ReadAsAsync<GameDto>().Result;
+            return View(selectedgame);
+        }
+
+        // POST: Game/Delete/3
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            string url = "gamedata/gameanimal/" + id;
+            HttpContent content = new StringContent("");
+            content.Headers.ContentType.MediaType = "application/json";
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
     }
 }
--- Games_Catalog_N01589651/Games_Catalog_N01589651/Controllers/GameDataController.cs
+++ Games_Catalog_N01589651/Games_Catalog_N01589651/Controllers/GameDataController.cs
@@ -1,10 +1,13 @@
 ﻿using Games_Catalog_N01589651.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace Games_Catalog_N01589651.Controllers
 {
@@ -36,13 +39,115 @@ namespace Games_Catalog_N01589651.Controllers
             return GameDtos;
         }
 
-        //Find Game
+        // GET: api/GameData/FindGame/2
+        [ResponseType(typeof(Game))]
+        [HttpGet]
+        [Route("api/gamedata/findgame/{id}")]
+        public IHttpActionResult FindGame(int id)
+        {
+            Game Game = db.Games.Find(id);
+            GameDto GameDto = new GameDto()
+            {
+                GameId = Game.GameId,
+                GameName = Game.GameName,
+                ReleaseDate = Game.ReleaseDate,
+                Price = Game.Price,
+                Description = Game.Description,
+                GenreName = Game.Genres.GenreName,
+                DeveloperName = Game.Developers.DeveloperStudioName
 
-        //Add Game
+            };
+            if (Game == null)
+            {
+                return NotFound();
+            }
 
-        //Update Game
+            return Ok(GameDto);
+        }
 
-        //Delete Game
+        // POST: api/GameData/UpdateGame/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateGame(int id, Game Game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != Game.GameId)
+            {
+
+                return BadRequest();
+            }
+
+            db.Entry(Game).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/GameData/AddGame
+        [ResponseType(typeof(Game))]
+        [HttpPost]
+        [Route("api/gamedata/addgame")]
+        public IHttpActionResult AddGame(Game Game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Games.Add(Game);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        // POST: api/GameData/DeleteGame/5
+        [ResponseType(typeof(Game))]
+        [HttpPost]
+        public IHttpActionResult DeleteGame(int id)
+        {
+            Game Game = db.Games.Find(id);
+            if (Game == null)
+            {
+                return NotFound();
+            }
+
+            db.Games.Remove(Game);
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool GameExists(int id)
+        {
+            return db.Games.Count(e => e.GameId == id) > 0;
+        }
 
     }
 }
